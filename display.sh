2c5a964c25cac4af429dc09b53784fdfa4dba884
timlindquist <timmylindquist@gmail.com>
2018-01-12 23:07:24 -0600

    first push

--- display.sh
+++ display.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 tvservice -n | grep "device_name" &> /dev/null
-if [ $? == 0]; then
+if [ $? == 0]; then           #HDMI connected
   echo "HDMI Connected"
-else
+  sudo cat /boot/config.txt | grep "ignore_lcd=1" &> /dev/null
+  if [ $? != 0 ]; then        #write ignore_lcd=1 and reboot
+    sudo sh -c "echo 'ignore_lcd=1' >> /boot/config.txt"
+    sudo reboot
+  fi
+else                          #HDMI disconnected
   echo "HDMI Disconnected"
+  sudo cat /boot/config.txt | grep "ignore_lcd=1" &> /dev/null
+  if [ $? == 0 ]; then        #delete ignore_lcd=1 and reboot
+    sudo sed -i '/ignore_lcd=1/,$d' /boot/config.txt
+    sudo reboot
+  fi
 fi

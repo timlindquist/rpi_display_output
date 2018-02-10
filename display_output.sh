49ef00df4f5590342f6cfe11f4f424a20dd8464d
timlindquist <timmylindquist@gmail.com>
2018-02-09 22:22:41 -0600

    Audio
    added audio HDMI/ 3.5mm automatic switching

--- display_output.sh
+++ display_output.sh
@@ -3,6 +3,7 @@
 tvservice -n | grep "device_name" &> /dev/null
 if [ $? == 0 ]; then           #HDMI connected
   echo "HDMI Connected"
+  amixer cset numid=3 2       #set HDMI as audio output
   sudo cat /boot/config.txt | grep "ignore_lcd=1" &> /dev/null
   if [ $? != 0 ]; then        #write ignore_lcd=1 and reboot
     sudo sh -c "echo 'ignore_lcd=1' >> /boot/config.txt"
@@ -10,6 +11,7 @@ if [ $? == 0 ]; then           #HDMI connected
   fi
 else                          #HDMI disconnected
   echo "HDMI Disconnected"
+  amixer cset numid=3 1       #set 3.5mm Headphone as audio output
   sudo cat /boot/config.txt | grep "ignore_lcd=1" &> /dev/null
   if [ $? == 0 ]; then        #delete ignore_lcd=1 and reboot
     sudo sed -i '/ignore_lcd=1/,$d' /boot/config.txt
